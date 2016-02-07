2e7e62725fac598f25bcf265fedbc97cc4dc81a7
Christopher <chris.morris55@yahoo.com>
2016-02-06 20:33:38 -0500

    lalal

--- webpage.py
+++ webpage.py
@@ -1,10 +1,18 @@
 from flask import render_template, Flask, request, json
+from database import *
 app = Flask(__name__, static_folder='/Users/Main_Account/Documents/GitHub/Coding_Game')
 
 @app.route('/')
 def home():
-    print("loading home")
-    return render_template('trash.html')
+    #print("loading home")
+    return render_template('index.html')
+
+@app.route('/_create_user', methods=["POST"])
+def create():
+    json = request.json
+    username = json["username"]
+    createUser(username)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
