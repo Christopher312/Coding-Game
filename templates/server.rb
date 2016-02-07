c6c8d84ec953bdc6f9873696d1d78d2be4152e5f
LogicalShark <marcusa3.14@gmail.com>
2016-02-07 01:17:24 -0500

    Delete server.rb

--- templates/server.rb
+++
@@ -1,9 +0,0 @@
-require 'rest-client'
-require 'json'
-
-CLIENT_ID = ENV['e101d89e649e0beae1eb']
-CLIENT_SECRET = ENV['2a2bf53ce7286342434fa49aed3e07d79f6aadd3']
-
-get '/' do
-  erb :index, :locals => {:client_id => e101d89e649e0beae1eb}
-end
