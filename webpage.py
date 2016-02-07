f4b395505ea2589e1a1259ef715cf63c026db349
Christopher <chris.morris55@yahoo.com>
2016-02-06 20:03:39 -0500

    temp try to break it

--- webpage.py
+++ webpage.py
@@ -1,10 +1,10 @@
 from flask import render_template, Flask, request, json
-app = Flask(__name__, static_folder='/Users/Main_Account/Documents/GitHub/Coding\-Game')
+app = Flask(__name__, static_folder='/Users/Main_Account/Documents/GitHub/Coding_Game')
 
 @app.route('/')
 def home():
     print("loading home")
-    return render_template('index.html')
+    return render_template('trash.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
